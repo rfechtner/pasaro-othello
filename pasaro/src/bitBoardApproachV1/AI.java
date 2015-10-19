97fc2b165b3f1ada421f7bf0144a395c6ce74c6f
paulhager <hagerp@zolling.cip.ifi.lmu.de>
2015-10-19 16:41:05 +0200

    Simple AI geschrieben

--- pasaro/src/bitBoardApproachV1/AI.java
+++ pasaro/src/bitBoardApproachV1/AI.java
@@ -2,46 +2,178 @@ package bitBoardApproachV1;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map.Entry;
+import java.util.Set;
 
 public class AI extends Player {
-		
+
+	private HashMap<Integer, Integer> moveScores = new HashMap<Integer, Integer>();
+	private HashMap<Integer, Integer> positionsMatrix = new HashMap<Integer, Integer>();
+	private long nextOwnChips;
+	private long nextOtherChips;
+	private int[][] posMatrixArray = new int[][]{
+		{  120, -20,  20,  5,  5,  20, -20, 120 },
+		{  -20, -40,  -5, -5, -5,  -5, -40, -20 },
+		{   20,  -5,  15,  3,  3,  15,  -5,  20 },
+		{   5,   -5,   3,  0,  0,   3,  -5,  5 },
+		{   5,   -5,   3,  0,  0,   3,  -5,  5 },
+		{   20,  -5,  15,  3,  3,  15,  -5,  20 },
+		{  -20, -40,  -5, -5, -5,  -5, -40, -20 },
+		{  120, -20,  20,  5,  5,  20, -20, 120 } };
+
 	public AI(ChipType chipType){
 		super(chipType, genOwnChips(chipType), genOtherChips(chipType));
+		//fills the hashmap with the desired score values for each position
+		int key = 0;
+		for(int y = 0;y < 8;y++){
+			for(int x = 0;x < 8; x++){
+				key = x+y*8;
+				positionsMatrix.put(key, posMatrixArray[x][y]);
+			}
+		}
 	}
-	
+
 	public void makeMove(int dezimal, HashMap<Integer, ArrayList<Integer>> toTurn) {
 		super.makeMove(dezimal, toTurn);
 	}
-	
-	public void makeMoveAI(HashMap<Integer, ArrayList<Integer>> toTurn){
-		
-		
-		int greed = berechneGreedy(toTurn);
-		
-		
-		makeMove(greed, toTurn);
-	}
-	
-	private int berechneGreedy(HashMap<Integer, ArrayList<Integer>> toTurn) {
-		
-		int biggest = 0;
-		int ausgabe = 0;
-		
-		for (Entry<Integer, ArrayList<Integer>> entry : toTurn.entrySet()) {
-		    Integer key = entry.getKey();
-		    ArrayList<Integer> value = entry.getValue();
-		    
-		    if(value.size() > biggest){
-		    	biggest = value.size();
-		    	ausgabe = key;
-		    }
-		    
+
+	/**
+	 * Updates scoring hash with new possible moves array and returns the position corresponding to the highest score --- the best move
+	 * @param possibleMoves - array of all possible moves
+	 * @return bestMove - position on board of best move calculated from best score
+	 */
+
+
+	public int getBestMove(Set<Integer> possibleMoves, int futTurns, HashMap<Integer, ArrayList<Integer>> toTurn){
+		int bestMove=0;
+		int bestScore=0;
+		calculateScores(possibleMoves, futTurns, toTurn);
+		//iterate through the best score hashmap selecting and storing the best score and move
+		for(Integer i : possibleMoves){
+			if(bestScore < moveScores.get(i)){
+				bestScore = moveScores.get(i);
+				bestMove = i;
+			}
 		}
-		
-		return ausgabe;
+		return bestMove;
 	}
 
+	/**
+	 * Update moveScores hashmap with scores for all possible moves
+	 * @param possibleMoves - array of all possible moves
+	 */
 
+	public void calculateScores(Set<Integer> possibleMoves, int futTurns, HashMap<Integer, ArrayList<Integer>> toTurn){
+		//fill hash loop with possible move as key and score as value
+		for(Integer i : possibleMoves){
+			int score = 0;
+			score += edgeScore(i);
+			score += turnScore(i, toTurn);
+			score += vulnerabilityScore(i, futTurns);
+			moveScores.put(i, score);
+		}
+	}
+
+	/**
+	 * Runs similar to getBestMove but returns the score associated with the best move rather than the move itself.
+	 * @param possibleMoves - array of all possible moves
+	 * @param futTurns - how many turns into the future the method should look
+	 * @return bestScore - the calculated best score
+	 */
+
+	public int getBestScore(Set<Integer> possibleMoves, int futTurns, HashMap<Integer, ArrayList<Integer>> toTurn){
+		int bestScore = Integer.MIN_VALUE;
+
+		calculateScores(possibleMoves, futTurns, toTurn);
+		//iterate through the best score hashmap selecting and storing the best score
+		for(Integer i : possibleMoves){
+			if(bestScore < moveScores.get(i)){
+				bestScore = moveScores.get(i);
+			}
+		}
+		return bestScore;
+	}
+
+	/**
+	 * Reads a value out of the positionsMatrix hashmap
+	 * @param move - the move being evaluated
+	 * @return corresponding points awarded/subtracted based on the values in the positionMatrix set when AI is initialized
+	 */
+	public int edgeScore(int move){
+		return positionsMatrix.get(move);
+	}
+
+	/**
+	 * Increases score by the amount of pieces the move would turn over
+	 * @param move - move to be executed
+	 * @return score
+	 */
+	public int turnScore(int move, HashMap<Integer, ArrayList<Integer>> toTurn){
+		int score=0;
+		for (Integer i : toTurn.get(move)) {
+		    score += i;
+		}
+		return score;
+	}
+
+	/**
+	 * Calculates the vulnerability each move would create. Calls up the scoring methods for the board one move in the future. Works recursively until futureTurns is depleted
+	 * @param move - move to be executed
+	 * @param futureTurns - how many turns the AI should look into the future
+	 * @return score
+	 */
+	public int vulnerabilityScore(int move, int futureTurns){
+		nextOwnChips = getOwnChips();
+		nextOtherChips = getOtherChips();
+		HashMap<Integer, ArrayList<Integer>> toTurn = new HashMap<Integer, ArrayList<Integer>>();
+		Set<Integer> nextPossMoves;
+		int nextMoveBestScore = Integer.MIN_VALUE;
+		int toBeSubtracted = 0;
+
+		toTurn = possibleMoves (nextOwnChips, nextOtherChips);
+		makeMoveFuture(move, toTurn);
+		toTurn = possibleMoves (nextOwnChips, nextOtherChips);
+		nextPossMoves = toTurn.keySet();
+		nextMoveBestScore = getBestScore(nextPossMoves, --futureTurns, toTurn);
+		if(nextMoveBestScore > 120){
+			toBeSubtracted = 50;
+		}else if(nextMoveBestScore > 50){
+			toBeSubtracted = 10;
+		}else if(nextMoveBestScore > 30){
+			toBeSubtracted = 5;
+		}else if(nextMoveBestScore > 10){
+			toBeSubtracted = 3;
+		}else{
+			toBeSubtracted = 0;
+		}
+		return toBeSubtracted;
+	}
+
+	/**
+	 * Functions identical to makeMove from Player class but works with future chips
+	 * @param dezimal - move to make
+	 * @param toTurn - current possible moves
+	 */
+	public void makeMoveFuture(int dezimal, HashMap<Integer, ArrayList<Integer>> toTurn) {
+
+		ArrayList<Integer> tmpArray = toTurn.get(dezimal);
+
+		long big = 0L;
+		long dezLong = (long) Math.pow(2, dezimal);
+
+		big = big | (1L << 63);
+
+		if(dezimal == 63){
+			nextOwnChips = nextOwnChips | big;
+		}else{
+			nextOwnChips = nextOwnChips | dezLong;
+		}
+
+		for(Integer i : tmpArray){
+			dezLong = (long) Math.pow(2, i);
+			nextOwnChips = nextOwnChips | dezLong;
+			nextOtherChips = nextOtherChips & ~(1L << i);
+		}
+
+	}
 
 }
--- pasaro/src/bitBoardApproachV1/Player.java
+++ pasaro/src/bitBoardApproachV1/Player.java
@@ -11,7 +11,7 @@ public abstract class Player {
 	private int score;
 	private long ownChips;
 	private long otherChips;
-	
+
 	private ArrayList<Integer> upLeft = new ArrayList<Integer>();
 	private ArrayList<Integer> up = new ArrayList<Integer>();
 	private ArrayList<Integer> upRight = new ArrayList<Integer>();
@@ -20,7 +20,7 @@ public abstract class Player {
 	private ArrayList<Integer> downLeft = new ArrayList<Integer>();
 	private ArrayList<Integer> down = new ArrayList<Integer>();
 	private ArrayList<Integer> downRight = new ArrayList<Integer>();
-	
+
 	private boolean gegnerDazwischen;
 	private long possibleMoves;
 
@@ -42,7 +42,7 @@ public abstract class Player {
 
 		return ownChips;
 	}
-	
+
 	public static long genOtherChips(ChipType chipType) {
 		long otherChips;
 
@@ -58,15 +58,15 @@ public abstract class Player {
 	public long getOwnChips() {
 		return ownChips;
 	}
-	
+
 	public void setOwnChips(long ownChips){
 		this.ownChips = ownChips;
 	}
-	
+
 	public long getOtherChips() {
 		return otherChips;
 	}
-	
+
 	public void setOtherChips(long otherChips){
 		this.otherChips = otherChips;
 	}
@@ -78,22 +78,21 @@ public abstract class Player {
 	public ChipType getChipType() {
 		return chipType;
 	}
-	
+
 	public void toTurnAusgeben(HashMap<Integer, ArrayList<Integer>> toTurn){
 		for (Entry<Integer, ArrayList<Integer>> entry : toTurn.entrySet()) {
 		    Integer key = entry.getKey();
 		    ArrayList<Integer> value = entry.getValue();
-		    
+
 		    System.out.println("\t\t\t\tKey: "+key+" , Value: "+value);
 		}
 	}
 
 	public HashMap<Integer, ArrayList<Integer>> possibleMoves(long ownchips, long otherchips) {
-		
 		HashMap<Integer, ArrayList<Integer>> toTurn = new HashMap<Integer, ArrayList<Integer>>();
-		
+
 		toTurn.clear();
-		
+
 		up.clear();
 		upLeft.clear();
 		upRight.clear();
@@ -102,23 +101,23 @@ public abstract class Player {
 		down.clear();
 		downLeft.clear();
 		downRight.clear();
-		
+
 		berechneZuege(otherchips, ownchips, toTurn);
-		
+
 		long ausgabe = 0L;
-		
+
 		for (Entry<Integer, ArrayList<Integer>> entry : toTurn.entrySet()) {
 		    Integer key = entry.getKey();
 		    ArrayList<Integer> value = entry.getValue();
-		    
+
 		    System.out.println("Moeglicher Zug (toTurn): "+key+" --> "+value);
-		    
+
 		}
-	
+
 		possibleMoves = ausgabe;
-		
+
 		return toTurn;
-		
+
 	}
 
 	public void berechneZuege(long gegner, long spieler, HashMap<Integer, ArrayList<Integer>> toTurn) {
@@ -143,23 +142,23 @@ public abstract class Player {
 		} else if (((gegner >> pos + 8) & 1) == 1) {
 
 			up.add(pos+8);
-			
+
 			gegnerDazwischen = true;
 			gehtUnten(gegner, spieler, pos + 8, toTurn);
-			
+
 		} else if (gegnerDazwischen && ((spieler >> pos + 8) & 1) != 1) {
 
-			if(!toTurn.containsKey(pos+8)){				
+			if(!toTurn.containsKey(pos+8)){
 				toTurn.put(pos+8, new ArrayList<Integer>());
 			}
-			
+
 			for(Integer i : up){
 				toTurn.get(pos+8).add(i);
 			}
 			up.clear();
-			
+
 			System.out.println("Umzudrehen fuer: "+(pos+8)+" "+toTurn.get(pos+8));
-			
+
 			gegnerDazwischen = false;
 		}else{
 			up.clear();
@@ -171,24 +170,24 @@ public abstract class Player {
 			right.clear();
 			return;
 		} else if (((gegner >> pos - 1) & 1) == 1) {
-			
+
 			right.add(pos-1);
-			
+
 			gegnerDazwischen = true;
 			gehtLinks(gegner, spieler, pos - 1, toTurn);
 		} else if (gegnerDazwischen && ((spieler >> pos - 1) & 1) != 1) {
 
-			if(!toTurn.containsKey(pos-1)){				
+			if(!toTurn.containsKey(pos-1)){
 				toTurn.put(pos-1, new ArrayList<Integer>());
 			}
-			
+
 			for(Integer i : right){
 				toTurn.get(pos-1).add(i);
 			}
 			right.clear();
-			
+
 			System.out.println("Umzudrehen fuer: "+(pos-1)+" "+toTurn.get(pos-1));
-			
+
 			gegnerDazwischen = false;
 		}else{
 			right.clear();
@@ -200,24 +199,24 @@ public abstract class Player {
 			upRight.clear();
 			return;
 		} else if (((gegner >> pos + 7) & 1) == 1) {
-						
+
 			upRight.add(pos+7);
-			
+
 			gegnerDazwischen = true;
 			gehtUntenLinks(gegner, spieler, pos + 7, toTurn);
 		} else if (gegnerDazwischen && ((spieler >> pos + 7) & 1) != 1) {
 
-			if(!toTurn.containsKey(pos+7)){				
+			if(!toTurn.containsKey(pos+7)){
 				toTurn.put(pos+7, new ArrayList<Integer>());
 			}
-			
+
 			for(Integer i : upRight){
 				toTurn.get(pos+7).add(i);
 			}
 			upRight.clear();
-			
+
 			System.out.println("Umzudrehen fuer: "+(pos+7)+" "+toTurn.get(pos+7));
-			
+
 			gegnerDazwischen = false;
 		}else{
 			upRight.clear();
@@ -229,24 +228,24 @@ public abstract class Player {
 			left.clear();
 			return;
 		} else if (((gegner >> pos + 1) & 1) == 1) {
-			
+
 			left.add(pos+1);
-			
+
 			gegnerDazwischen = true;
 			gehtRechts(gegner, spieler, pos + 1, toTurn);
 		} else if (gegnerDazwischen && ((spieler >> pos + 1) & 1) != 1) {
 
-			if(!toTurn.containsKey(pos+1)){				
+			if(!toTurn.containsKey(pos+1)){
 				toTurn.put(pos+1, new ArrayList<Integer>());
 			}
-			
+
 			for(Integer i : left){
 				toTurn.get(pos+1).add(i);
 			}
 			left.clear();
-			
+
 			System.out.println("Umzudrehen fuer: "+(pos+1)+" "+toTurn.get(pos+1));
-			
+
 			gegnerDazwischen = false;
 		}else{
 			left.clear();
@@ -258,24 +257,24 @@ public abstract class Player {
 			upLeft.clear();
 			return;
 		} else if (((gegner >> pos + 9) & 1) == 1) {
-			
+
 			upLeft.add(pos+9);
-			
+
 			gegnerDazwischen = true;
 			gehtUntenRechts(gegner, spieler, pos + 9, toTurn);
 		} else if (gegnerDazwischen && ((spieler >> pos + 9) & 1) != 1) {
 
-			if(!toTurn.containsKey(pos+9)){				
+			if(!toTurn.containsKey(pos+9)){
 				toTurn.put(pos+9, new ArrayList<Integer>());
 			}
-			
+
 			for(Integer i : upLeft){
 				toTurn.get(pos+9).add(i);
 			}
 			upLeft.clear();
-			
+
 			System.out.println("Umzudrehen fuer: "+(pos+9)+" "+toTurn.get(pos+9));
-			
+
 			gegnerDazwischen = false;
 		}else{
 			upLeft.clear();
@@ -287,24 +286,24 @@ public abstract class Player {
 			down.clear();
 			return;
 		} else if (((gegner >> pos - 8) & 1) == 1) {
-			
+
 			down.add(pos-8);
-			
+
 			gegnerDazwischen = true;
 			getOben(gegner, spieler, pos - 8, toTurn);
 		} else if (gegnerDazwischen && ((spieler >> pos - 8) & 1) != 1) {
 
-			if(!toTurn.containsKey(pos-8)){				
+			if(!toTurn.containsKey(pos-8)){
 				toTurn.put(pos-8, new ArrayList<Integer>());
 			}
-			
+
 			for(Integer i : down){
 				toTurn.get(pos-8).add(i);
 			}
 			down.clear();
-			
+
 			System.out.println("Umzudrehen fuer: "+(pos-8)+" "+toTurn.get(pos-8));
-			
+
 			gegnerDazwischen = false;
 		}else{
 			down.clear();
@@ -316,24 +315,24 @@ public abstract class Player {
 			downLeft.clear();
 			return;
 		} else if (((gegner >> pos - 7) & 1) == 1) {
-			
+
 			downLeft.add(pos-7);
-			
+
 			gegnerDazwischen = true;
 			gehtObenRechts(gegner, spieler, pos - 7, toTurn);
 		} else if (gegnerDazwischen && ((spieler >> pos - 7) & 1) != 1) {
 
-			if(!toTurn.containsKey(pos-7)){				
+			if(!toTurn.containsKey(pos-7)){
 				toTurn.put(pos-7, new ArrayList<Integer>());
 			}
-			
+
 			for(Integer i : downLeft){
 				toTurn.get(pos-7).add(i);
 			}
 			downLeft.clear();
-			
+
 			System.out.println("Umzudrehen fuer: "+(pos-7)+" "+toTurn.get(pos-7));
-			
+
 			gegnerDazwischen = false;
 		}else{
 			downLeft.clear();
@@ -345,24 +344,24 @@ public abstract class Player {
 			downRight.clear();
 			return;
 		} else if (((gegner >> pos - 9) & 1) == 1) {
-			
+
 			downRight.add(pos-9);
-			
+
 			gegnerDazwischen = true;
 			gehtObenLinks(gegner, spieler, pos - 9, toTurn);
 		} else if (gegnerDazwischen && ((spieler >> pos - 9) & 1) != 1) {
 
-			if(!toTurn.containsKey(pos-9)){				
+			if(!toTurn.containsKey(pos-9)){
 				toTurn.put(pos-9, new ArrayList<Integer>());
 			}
-			
+
 			for(Integer i : downRight){
 				toTurn.get(pos-9).add(i);
 			}
 			downRight.clear();
-			
+
 			System.out.println("Umzudrehen fuer: "+(pos-9)+" "+toTurn.get(pos-9));
-			
+
 			gegnerDazwischen = false;
 		}else{
 			downRight.clear();
@@ -372,24 +371,24 @@ public abstract class Player {
 	public void makeMove(int dezimal, HashMap<Integer, ArrayList<Integer>> toTurn) {
 
 		ArrayList<Integer> tmpArray = toTurn.get(dezimal);
-		
+
 		long big = 0L;
 		long dezLong = (long) Math.pow(2, dezimal);
-		
+
 		big = big | (1L << 63);
-		
+
 		if(dezimal == 63){
 			ownChips = ownChips | big;
 		}else{
 			ownChips = ownChips | dezLong;
 		}
-		
+
 		for(Integer i : tmpArray){
 			dezLong = (long) Math.pow(2, i);
 			ownChips = ownChips | dezLong;
 			otherChips = otherChips & ~(1L << i);
 		}
-		
+
 	}
 
 	/* TEST */
@@ -398,7 +397,7 @@ public abstract class Player {
 		for (int i = 0; i < 64; i++) {
 			spielfeld[i / 8][i % 8] = " ";
 		}
-		
+
 		for (int i = 0; i < 64; i++) {
 			if (((schwarz >> i) & 1) == 1) {
 				spielfeld[i / 8][i % 8] = "s";
