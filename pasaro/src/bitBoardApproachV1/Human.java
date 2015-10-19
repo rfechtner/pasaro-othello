994d98649b8de97d04e023b0fc3b986f622c1320
rfechtner <ronfechtner@gmail.com>
2015-10-19 13:24:29 +0200

    nush

--- pasaro/src/bitBoardApproachV1/Human.java
+++ pasaro/src/bitBoardApproachV1/Human.java
@@ -2,6 +2,8 @@ package bitBoardApproachV1;
 
 public class Human extends Player {
 	
+	/* Test */
+	
 	public Human(ChipType chipType){
 		super(chipType, genOwnChips(chipType), genOtherChips(chipType));
 	}
